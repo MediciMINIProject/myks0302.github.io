d465c4a1c66c4c69199e8ac93e7da67164f1041f
Kim Seong <stariker8@gmail.com>
2022-08-03 16:10:59 +0900

    전개도 및 HP 시스템 구현

---
+++ Assets/Scripts/Entitys/HP.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP : MonoBehaviour
+{
+    int hitpoint; // 현재 체력
+    public int HITPOINT
+    {
+        get { return hitpoint; }
+
+        set 
+        {
+            hitpoint = value;
+        }
+    }
+
+    int starthp; //시작 체력 및 최대 체력
+
+    public int STARTHP
+    {
+        get { return starthp; }
+
+        set
+        {
+            starthp = value;
+        }
+    } //공통 변수
+
+    public void TakeDamage(int damage) //데미지 받는 메소드
+    {
+        hitpoint -= damage;
+    }
+
+    private void Start()
+    {
+        hitpoint = starthp; //시작 체력을 현재 체력 값에 부여
+    }
+}
